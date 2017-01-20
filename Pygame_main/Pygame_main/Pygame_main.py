46f38b0695f707137774e10d54fcf2c8c8dd4442
KimYiuLui <kimyiu-lui@hotmail.com>
2017-01-20 16:18:13 +0100

    Add files via upload

--- Pygame_main/Pygame_main/Pygame_main.py
+++ Pygame_main/Pygame_main/Pygame_main.py
@@ -1,6 +1,8 @@
 import pygame, pygame.mixer ,sys, math, time, random
 
 pygame.init()
+
+#colours
 black = (0, 0, 0)
 gray = (120, 120, 120)
 white = (255, 255, 255)
@@ -11,63 +13,57 @@ d_red = (139,0,0)
 gold = (255, 215,0)
 green = (0,128,0)
 d_blue = (30, 114, 225)
- 
+
+#screen size
 width = 1280
 height = 720
 size = (width,height)
- 
-black = (0,0,0)
-white = (255,255,255)
-red = (255,0,0)
- 
-gameDisplay = pygame.display.set_mode((size))
+
+#screen 
+screen = pygame.display.set_mode((size))
 pygame.display.set_caption('Beklim de Euromast')
 clock = pygame.time.Clock()
 
-
+#text for button
 def text_objects(text, font):
     textSurface = font.render(text, True, d_red)
     return textSurface, textSurface.get_rect()
- 
+
+# text on display(large text) 
 def message_display(text):
     largeText = pygame.font.Font('freesansbold.ttf',115)
     TextSurf, TextRect = text_objects(text, largeText)
     TextRect.center = ((display_width/2),(display_height/2))
-    gameDisplay.blit(TextSurf, TextRect)
+    screen.blit(TextSurf, TextRect)
  
     pygame.display.update()
  
     time.sleep(2)
  
-    game_loop()
-
+#buttons 
 def button(msg,x,y,w,h,ic,ac,action=None):
     mouse = pygame.mouse.get_pos()
     click = pygame.mouse.get_pressed()
     print(click)
     if x+w > mouse[0] > x and y+h > mouse[1] > y:
-        pygame.draw.rect(gameDisplay, ac,(x,y,w,h))
+        pygame.draw.rect(screen, ac,(x,y,w,h))
         
         if click[0] == 1 and action != None:
             action()   
                   
     else:
-        pygame.draw.rect(gameDisplay, ic,(x,y,w,h))
+        pygame.draw.rect(screen, ic,(x,y,w,h))
         
-
-
     smallText = pygame.font.SysFont("Arial black",20)
     textSurf, textRect = text_objects(msg, smallText)
     textRect.center = ( (x+(w/2)), (y+(h/2)) )
-    gameDisplay.blit(textSurf, textRect)
+    screen.blit(textSurf, textRect)
     
-
-
 def quitgame():
     pygame.quit()
     quit()
 
-
+#main menu
 def game_intro():
 
     intro = True
@@ -79,12 +75,12 @@ def game_intro():
                 pygame.quit()
                 quit()
         bg = pygame.image.load("euromast_wallpaper_v2.jpg")   
-        gameDisplay.fill(white)
-        gameDisplay.blit(bg, (0, 0)) 
-        largeText = pygame.font.SysFont("comicsansms",115)
+        screen.fill(white)
+        screen.blit(bg, (0, 0)) 
+        largeText = pygame.font.SysFont("calibri",115)
 
         button("Play",1050,150,150,50,black,(80,80,80),None)
-        button("Intructie",1050,250,150,50,black,(80,80,80),instructies)
+        button("Instructie",1050,250,150,50,black,(80,80,80),instructies)
         button("High Score",1050,350,150,50,black,(80,80,80),None)
         button("Option",1050,450,150,50,black,(80,80,80),None)
         button("Quit",1050,550,150,50,black,(80,80,80),quitgame)
@@ -93,12 +89,23 @@ def game_intro():
         clock.tick(15)
 
 
+def previous():
+    key = pygame.key.get_pressed()
+    if button("<",940,40,50,50,black,(80,80,80),None) == True:
+        button.action = key[pygame.K_RIGHT]
+        
+
+    
+
+#insturction page
 def instructies():
     pygame.display.set_caption("Instruction Screen")
     done = False
     display_instructions = True
     instruction_page = 1
-
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+    print(click)
     # LOOP INSTRUCTIEPAGINA
     while not done and display_instructions:
         for event in pygame.event.get():
@@ -115,33 +122,36 @@ def instructies():
                     instruction_page = 3
 
         # Set the screen background
-        gameDisplay.fill(black)
+        screen.fill(black)
 
         if instruction_page == 1:
             # Draw instructions, page 1
             instruc1 = pygame.image.load("instructiepagina1.jpg")
             instruc1 = pygame.transform.scale(instruc1, (size))
-            gameDisplay.blit(instruc1,(0,0))
+            screen.blit(instruc1,(0,0))
 
         if instruction_page == 2:
             # Draw instructions, page 1
             instruc2 = pygame.image.load("instructiepagina2.jpg")
             instruc2 = pygame.transform.scale(instruc2, (size)) 
-            gameDisplay.blit(instruc2,(0,0))
+            screen.blit(instruc2,(0,0))
 
         if instruction_page == 3:
             # Draw instructions, page 1
             instruc3 = pygame.image.load("instructiepagina3.jpg")
             instruc3 = pygame.transform.scale(instruc3, (size))
-            gameDisplay.blit(instruc3,(0,0))
+            screen.blit(instruc3,(0,0))        
+
 
         button("<",940,40,50,50,black,(80,80,80),None)
-        button(">",1170,40,50,50,black,(80,80,80),None)
+        button(">",1170,40,50,50,black,(80,80,80), None)
         button("Back To menu",1000,40,160,50,black,(80,80,80),game_intro)
 
-        # Go ahead and update the screen with what we've drawn.
+        # Go ahead and update the screen with what we've drawn.        
         pygame.display.flip()
+    
 
+    
 
 game_intro()
 pygame.quit()
